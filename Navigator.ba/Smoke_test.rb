d0193786f54b6449edd87c2a76fa21fa1928b7e0
medzidamustafic <47575256+medzidamustafic@users.noreply.github.com>
2019-03-06 00:49:07 +0100

    Add files via upload

--- Navigator.ba/Smoke_test.rb
+++ Navigator.ba/Smoke_test.rb
@@ -1,17 +1,17 @@
 require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 
 Capybara.default_driver = :selenium_chrome
+Capybara.ignore_hidden_elements = false
 
 RSpec.describe 'Smoke test', type: :feature do
 
-    it 'search page' do
-      visit 'https://www.navigator.ba/#/categories'
-      expect(page).to have_text('SARAJEVO')
-    end
+  before(:each) do
+    visit 'https://www.navigator.ba/#/categories'
+  end
 
-    it 'search place' do
-      visit 'https://www.navigator.ba/#/categories'
+    it 'search place'  do
       fill_in "Traži ulicu ili objekat", with: 'Kamerni teatar 55'
       find('.iconav-search').click
       find('.mCSB_container').click
@@ -20,7 +20,6 @@ RSpec.describe 'Smoke test', type: :feature do
     end
 
     it 'send message' do
-      visit 'https://www.navigator.ba/#/categories'
       find('#ember581').click
       fill_in "Ime i prezime", with: 'Medzida Mustafic'
       fill_in "Email", with: 'medzidamustafic@gmail.com'
@@ -30,21 +29,21 @@ RSpec.describe 'Smoke test', type: :feature do
       expect(page).to have_content "Hvala na poruci! Potrudit ćemo se da što prije reagujemo."
     end
 
-   it 'create place' do
-      visit 'https://www.navigator.ba/#/categories'
+   it 'create place'  do
       find('#ember566').click
-      fill_in 'poi_name', with: 'Biljna apoteka'
-      fill_in "poi_city_name", with: 'Sarajevo'
-      fill_in "poi_zip_code", with: '71000'
-      fill_in "poi_place_id", with: 'Aleja Lipa'
-      fill_in "poi_house_number", with: '33'
-      fill_in "poi_description", with: 'Domaća biljna apoteka'
-      find_button('Odaberite kategoriju').click
-      find('.span3 option[value="11"]').select_option
-      find('.span3 option[value="203"]').select_option
-      execute_script("arguments[0].scrollBy(84,294);", page.find("button.btn-success", visible: false).native)
-      click_button 'Kreiraj'
-      expect(page).to have_css ".leaflet-popup"
+      within('#place-form') do
+        fill_in 'poi_name', with: 'Biljna apoteka'
+        fill_in "poi_city_name", with: 'Sarajevo'
+        fill_in "poi_zip_code", with: '71000'
+        fill_in "poi_place_id", with: 'Aleja Lipa'
+        fill_in "poi_house_number", with: '33'
+        fill_in "poi_description", with: 'Domaća biljna apoteka'
+        find_button('Odaberite kategoriju').click
+        find('.span3 option[value="11"]').select_option
+        find('.span3 option[value="203"]').select_option
+        element=find_button("Kreiraj", visible: false)
+        execute_script("arguments[0].click();", page.find("button.btn-success", visible: false))
+       end
+     expect(page).to have_text "Biljna apoteka"
     end
-
 end    
